cce19b48066937e0bb4981ed93e4141946f73962
JordanRendon <jordanre7991@gmail.com>
2022-10-03 09:58:06 -0500

    se agrego btn ir arriba

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 const navMenuLogo = document.querySelector('.nav-menu-logo')
 const navMenu = document.querySelector('.nav-menu')
+const btnIrArriba = document.querySelector('.btn-ir-arriba')
 
 navMenuLogo.addEventListener('click', ()=>{
     navMenu.classList.toggle('active')
+})
+
+btnIrArriba.addEventListener('click',()=>{
+    document.documentElement.scrollTop = 0;
+})
+
+window.addEventListener('scroll', ()=>{
+    if(window.scrollY < 500){
+        btnIrArriba.style.right = -100 + 'px'
+    }else{
+        btnIrArriba.style.right = 5 + 'px'
+    }
 })
