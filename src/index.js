8f8b9298979680cbbf2ed2b8b7432d970ac9b21a
JordanRendon <jordanre7991@gmail.com>
2022-10-06 10:22:03 -0500

    Portafolio Terminado

--- src/index.js
+++ src/index.js
@@ -21,6 +21,20 @@ window.addEventListener('scroll', ()=>{
 
 btnModoOscuro.addEventListener('click',()=>{
     document.body.classList.toggle('dark')
-    
     btnModoOscuro.classList.toggle('active')
+
+    if(document.body.classList.contains('dark')){
+        localStorage.setItem('dark-mode', 'true')
+    }else{
+        localStorage.setItem('dark-mode','false')
+    }
+
 })
+
+if(localStorage.getItem('dark-mode')==='true'){
+    document.body.classList.add('dark')
+    btnModoOscuro.classList.add('active')
+}else{
+    document.body.classList.remove('dark')
+    btnModoOscuro.classList.remove('active')
+}
